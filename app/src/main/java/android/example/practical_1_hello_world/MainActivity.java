06a3dd9b241dd480973e1caeb9a9e72ce44e319a
WZX <whizzzx_@hotmail.com>
2020-06-18 15:22:47 +0800

    Hello World by WZX

---
+++ app/src/main/java/android/example/practical_1_hello_world/MainActivity.java
@@ -0,0 +1,15 @@
+package android.example.practical_1_hello_world;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
